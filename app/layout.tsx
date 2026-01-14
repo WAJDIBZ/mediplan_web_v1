fa6fb84ebedf56c8e2f6b4ca82d8b7bdbd6f9246
WAJDIBZ <benzinebwajdi@rades.r-iset.tn>
2026-01-14 17:09:32 +0100

    money

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,15 +41,18 @@ export default function RootLayout({
         <AppProviders>
           <GlobalShell>{children}</GlobalShell>
         </AppProviders>
+
+        {/* CORRECT SCRIPT */}
+        {/* Added id="monetag-vignette-injected" so the bot detects it naturally */}
         <Script
           src="https://gizokraijaw.net/vignette.min.js"
           data-zone="10444145"
+          id="monetag-vignette-injected"
+          strategy="afterInteractive"
         />
-        <Script
-          src="https://quge5.com/88/tag.min.js"
-          data-zone="201993"
-          data-cfasync="false"
-        />
+
+        {/* CONFLICTING SCRIPT REMOVED */}
+        {/* The quge5.com script was causing the 'adex timeout' conflict */}
       </body>
     </html>
   );
